45774864c658976c0b68348c08f6eb1bf279e768
Kidd Dawny Lu <kidd.dawny.lu@gmail.com>
2016-08-12 18:00:59 +0800

    update for elf download
    Signed-off-by: Kidd Dawny Lu <kidd.dawny.lu@gmail.com>

--- ramparser_msm8974.bat
+++ ramparser_msm8974.bat
@@ -1,4 +1,4 @@
-@echo off
+@echo on
 
 set CUR_PATH=%~dp0
 
@@ -13,27 +13,30 @@ set CURL_EXE=%CUR_PATH%\toolkit\win32\curl.exe
 
 set FIND_CGI=http://172.16.2.18/cgi-bin/vmlinux-lookup.cgi
 
-if not exist %CUR_PATH%\vmlinux (
-	goto NOVMLINUX 
-) else (
-	goto HAVEVMLINUX
-)
-
-:NOVMLINUX
-echo No vmlinux, find it here
-echo.
+:: Linux version
 %BUSYBOX_EXE% dd if=%CUR_PATH%\DDRCS0.BIN bs=20M count=1 2>nul | %BUSYBOX_EXE% strings | %BUSYBOX_EXE% grep "Linux version" | %BUSYBOX_EXE% head -n 1 | clip.exe
 for /f "delims=" %%i in (' %PASTE_EXE% ') do (set LINUX_VER=%%i)
 echo %LINUX_VER%
 
+:: Kernel 
 %CURL_EXE% --data-urlencode "version=%LINUX_VER%" %FIND_CGI% 2>nul | %BUSYBOX_EXE% grep "kernel symbols" -A 1 | %BUSYBOX_EXE% tail -1 | ^
 %BUSYBOX_EXE% sed "s/smb:\/\//\\\\/g" | %BUSYBOX_EXE% sed "s/\//\\/g" | clip.exe
 for /f "delims=" %%i in (' %PASTE_EXE% ') do (set SMB_PATH=%%i)
 echo %SMB_PATH%
-copy /y %SMB_PATH%\vmlinux %CUR_PATH%\vmlinux
+if not exist %CUR_PATH%\vmlinux (
+	copy /y %SMB_PATH%\vmlinux %CUR_PATH%\vmlinux
+)
+
+:: OEM
+%CURL_EXE% --data-urlencode "version=%LINUX_VER%" %FIND_CGI% 2>nul | %BUSYBOX_EXE% grep "OEM symbols" -A 1 | %BUSYBOX_EXE% tail -1 | ^
+%BUSYBOX_EXE% sed "s/smb:\/\//\\\\/g" | %BUSYBOX_EXE% sed "s/\//\\/g" | clip.exe
+for /f "delims=" %%i in (' %PASTE_EXE% ') do (set SMB_PATH=%%i)
+echo %SMB_PATH%
+copy /y %SMB_PATH%\oem-code\trustzone_images\core\securemsm\trustzone\qsee\tz.elf %CUR_PATH%\tzelf
+copy /y %SMB_PATH%\oem-code\rpm_proc\core\bsp\rpm\build\RPM_AAAAANAAR.elf %CUR_PATH%\rpmelf
 
 
-:HAVEVMLINUX
+::Start parse
 echo msm8974 ram parser Linux start
 %BUSYBOX_EXE% rm -rf %CUR_PATH%\ap-log
 %BUSYBOX_EXE% mkdir %CUR_PATH%\ap-log
